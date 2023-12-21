517efd1ce2b2a162b69462c0ba0eda3c86168226
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-21 16:56:55 +0100

    added message & number check

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 const playerOne = Math.floor(Math.random() * 6) + 1;
 const playerTwo = Math.floor(Math.random() * 6) + 1;
 console.log(playerOne, playerTwo);
+
+
+let message = '';
+
+// Confrontiamoli tra di loro per stabilire chi fa il punteggio più alto
+if (playerOne === playerTwo) {
+    message += 'La partita finisce in parità!'
+} else if (playerOne > playerTwo) {
+    message += 'Il player uno è il vincitore!'
+} else {
+    message += 'Il player due è il vincitore!'
+}

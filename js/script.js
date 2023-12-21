f0b6597bd4e6a77ba47182985b6ed09900881fa5
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-21 17:18:26 +0100

    printed text message & refactoring js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Prendiamo gli elementi dall HTML
 const playerOne = document.getElementById('player-one');
 const playerTwo = document.getElementById('player-two');
+const result = document.getElementById('result');
 
 // Generiamo 2 numeri casuali da 1 a 6
 const numberPlayerOne = Math.floor(Math.random() * 6) + 1;
 const numberPlayerTwo = Math.floor(Math.random() * 6) + 1;
-console.log(numberPlayerOne, numberPlayerTwo);
 
 let message = '';
 
@@ -18,8 +18,9 @@ if (numberPlayerOne === numberPlayerTwo) {
     message += 'Il player due è il vincitore!'
 }
 
-console.log(message);
-
 // Stampiamo in pagina i due tiri
 playerOne.innerHTML = `Il numero del player 1 è: <strong>${numberPlayerOne}</strong>`;
 playerTwo.innerHTML = `Il numero del player 1 è: <strong>${numberPlayerTwo}</strong>`;
+
+// Stampiamo in pagina il risultato
+result.innerText = message;

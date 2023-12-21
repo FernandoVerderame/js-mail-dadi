3e6451bb9b95e57942d9ae8cac70cd10e18c7bd4
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-21 16:42:16 +0100

    random numbers

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Generiamo 2 numeri casuali da 1 a 6
+const playerOne = Math.floor(Math.random() * 6) + 1;
+const playerTwo = Math.floor(Math.random() * 6) + 1;
+console.log(playerOne, playerTwo);

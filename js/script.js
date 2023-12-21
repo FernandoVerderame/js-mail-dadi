305587c65cd1a5da7595261669fa346c0b65d5d0
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-21 17:06:41 +0100

    added elements into html & refactoring variables names

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-// Generiamo 2 numeri casuali da 1 a 6
-const playerOne = Math.floor(Math.random() * 6) + 1;
-const playerTwo = Math.floor(Math.random() * 6) + 1;
-console.log(playerOne, playerTwo);
+// Prendiamo gli elementi dall HTML
+const playerOne = document.getElementById('player-one');
+const playerTwo = document.getElementById('player-two');
 
+// Generiamo 2 numeri casuali da 1 a 6
+const numberPlayerOne = Math.floor(Math.random() * 6) + 1;
+const numberPlayerTwo = Math.floor(Math.random() * 6) + 1;
+console.log(numberPlayerOne, numberPlayerTwo);
 
 let message = '';
 
 // Confrontiamoli tra di loro per stabilire chi fa il punteggio più alto
-if (playerOne === playerTwo) {
+if (numberPlayerOne === numberPlayerTwo) {
     message += 'La partita finisce in parità!'
-} else if (playerOne > playerTwo) {
+} else if (numberPlayerOne > numberPlayerTwo) {
     message += 'Il player uno è il vincitore!'
 } else {
     message += 'Il player due è il vincitore!'
 }
+
+console.log(message);
+
